2d0e82f88da163d24f0bd374d9d7c7dab3e3b6cd
Shaheer Islam <shaheer.muzaffar.7@gmail.com>
2025-10-10 17:29:33 +0500

    Delete src/components/ui/TickButton directory

--- src/components/ui/TickButton/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * TickButton Component
- * A reusable button component that shows a tick icon after click
- */
-
-export { default as TickButton } from "./tickButton";
-export type { TickButtonProps } from "./tickButton";
-export { default } from "./tickButton";
